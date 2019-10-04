25ef7e1e0ea821f9dd5d9f814f557067eb6ec696
caroline <caroline.violot@epfl.ch>
2019-10-04 12:51:25 +0200

    Ultimate Answer

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello sweng");
+        System.out.println("Ultimate answer : "+ computeUltimateAnswer());
+    }
+    public static int computeUltimateAnswer(){
+        return 6 * 7;
     }
 }
